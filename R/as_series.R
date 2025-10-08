2e20cf98dca2df114aa24d8d10f7880cab5bd3c5
Tymoteusz Makowski <tymoteusz.makowski@appsilon.com>
2025-10-08 14:16:13 +0200

    feat: include error bars in the series object

--- R/as_series.R
+++ R/as_series.R
@@ -75,10 +75,27 @@ as_series <- function(x, x_class, y_class, sheetname = "sheet1") {
       label_serie <- label_ref(values = dataset[[l_colname]], region = label_serie_range)
     }
 
+    error_bars <- list(y = NULL)
+    if (!is.null(x$error_bars$y$lower)) {
+      y_lower_colname <- "Lower error bar"
+      if (y_colname != x$y) y_lower_colname <- paste(y_lower_colname, y_colname, sep = "_")
+
+      y_lower_range <- col_to_values_region(dataset, sheetname, y_lower_colname)
+      error_bars$y$lower <- num_ref(values = dataset[[y_lower_colname]], region = y_lower_range)
+    }
+    if (!is.null(x$error_bars$y$upper)) {
+      y_upper_colname <- "Upper error bar"
+      if (y_colname != x$y) y_upper_colname <- paste(y_upper_colname, y_colname, sep = "_")
+
+      y_upper_range <- col_to_values_region(dataset, sheetname, y_upper_colname)
+      error_bars$y$upper <- num_ref(values = dataset[[y_upper_colname]], region = y_upper_range)
+    }
+
     ser <- list(
       idx = length(series), order = length(series),
       tx = serie_name,
       x = x_serie, y = y_serie, label = label_serie,
+      error_bars = error_bars,
       stroke = x$series_settings$colour[y_colname],
       fill = x$series_settings$fill[y_colname],
       symbol = x$series_settings$symbol[y_colname],
